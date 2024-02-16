dd819477210ff76ddf0bd0c3def2fb9416787158
thosijulio <thosijulio@gmail.com>
2024-02-16 14:11:35 -0300

    FEAT - configuracao de middleware de erro

---
+++ src/helpers/CustomizedError.ts
@@ -0,0 +1,42 @@
+import { StatusCodes } from 'http-status-codes';
+
+class CustomizedError extends Error {
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class BadRequestError extends CustomizedError {
+  constructor(message: string) {
+    super(message, StatusCodes.BAD_REQUEST);
+  }
+}
+
+export class UnauthorizedError extends CustomizedError {
+  constructor(message: string) {
+    super(message, StatusCodes.UNAUTHORIZED);
+  }
+}
+
+export class ForbiddenError extends CustomizedError {
+  constructor(message: string) {
+    super(message, StatusCodes.FORBIDDEN);
+  }
+}
+
+export class NotFoundError extends CustomizedError {
+  constructor(message: string) {
+    super(message, StatusCodes.NOT_FOUND);
+  }
+}
+
+export class UnprocessableEntityError extends CustomizedError {
+  constructor(message: string) {
+    super(message, StatusCodes.UNPROCESSABLE_ENTITY);
+  }
+}
+
+export default CustomizedError;
--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import ErrorType from '../types/Error';
 import { StatusCodes } from 'http-status-codes';
 import getMessages from '../locales';
 import getLanguage from '../utils/getLanguage';
+import CustomizedError from '../helpers/CustomizedError';
 
-const errorMiddleware = (err: ErrorType, req: Request, res: Response, _next: NextFunction) => {
+const errorMiddleware = (error: Error & Partial<CustomizedError>, req: Request, res: Response, _next: NextFunction) => {
   const language = getLanguage(req.headers.language);
 
-  res
-    .status(err.status || StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: err.message || getMessages(language).errors.unknownError });
+  const message = error.statusCode ? error.message : getMessages(language).errors.unknownError;
+  const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+
+  res.status(statusCode).json({ message });
 };
 
 export default errorMiddleware;
